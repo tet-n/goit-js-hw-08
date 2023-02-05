abee205ef92659b3306e7abe60b81d167ffceec6
Tetiana Nivchyk <tata.02od@gmail.com>
2023-02-05 14:33:42 +0200

    solved 2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+var iframe = document.querySelector('iframe');
+
+// Створюємо екземпляр Класу
+var player = new Player(iframe);
+const STORAGE_TIME_KEY = 'videoplayer-current-time';
+
+// Записуємо поточний час у сховище
+const onPlayPlayer = function (e) {
+  const timeWasPlayed = e.seconds;
+  localStorage.setItem(STORAGE_TIME_KEY, timeWasPlayed);
+
+  // Реалізація від себе. Якщо дійшло до кінця, то у разі перезавантаження відтворення відео почнеться спочатку.
+  // e.seconds === e.duration && localStorage.setItem(STORAGE_TIME_KEY, '0');
+};
+
+player.on('timeupdate', throttle(onPlayPlayer, 1000));
+
+// Відтворення відео під час перезавантаження з поточного часу
+const currentTime = localStorage.getItem(STORAGE_TIME_KEY);
+player.setCurrentTime(currentTime);
